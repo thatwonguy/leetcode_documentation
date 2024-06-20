9847f2cef778b66dc5c8c9c7dcdacd654d8cb190
thatwonguy <monsore1337@gmail.com>
2024-06-20 04:47:42 -0700

    problem and solutions added

---
+++ Problem_List/219_contains_duplicates_II.py
@@ -0,0 +1,39 @@
+# 219. Contains Duplicate II
+# Solved
+# Easy
+# Topics
+# Companies
+# Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,1], k = 3
+# Output: true
+# Example 2:
+
+# Input: nums = [1,0,1,1], k = 1
+# Output: true
+# Example 3:
+
+# Input: nums = [1,2,3,1,2,3], k = 2
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# 0 <= k <= 105
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        num_indices = {}
+
+        for index, num in enumerate(nums):
+            if num in num_indices and index - num_indices[num] <=k:
+                return True
+
+            num_indices[num] = index
+        return False
